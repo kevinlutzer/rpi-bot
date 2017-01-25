e5f8b9bd1882e210f77b2957d26953142c3e0438
Kevin Lutzer <kml183@mail.usask.ca>
2017-01-25 03:30:49 +0000

    updated motor pinouts

--- ssh_control_motor/ssh_control_motor.py
+++ ssh_control_motor/ssh_control_motor.py
@@ -6,6 +6,7 @@
 import RPi.GPIO as GPIO
 import time
 import sys
+from curtsies import Input
 
 #gpio pin connections to the h-bridge
 MOTORL_N = 5
@@ -13,7 +14,7 @@ MOTORL_P = 6
 MOTORR_P = 13
 MOTORR_N = 19
 
-#function to init the gpio states as well as the BCM python module
+# function to init the gpio states as well as the BCM python module
 def init_gpio():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(MOTORL_N, GPIO.OUT)
@@ -22,7 +23,7 @@ def init_gpio():
     GPIO.setup(MOTORR_P, GPIO.OUT)
 
 # function to make the robot go forward
-def go_fwd():
+def go_bwd():
     GPIO.output(MOTORL_N, GPIO.HIGH)
     GPIO.output(MOTORL_P, GPIO.LOW)
     GPIO.output(MOTORR_N, GPIO.HIGH)
@@ -35,9 +36,52 @@ def stop_m():
     GPIO.output(MOTORR_N, GPIO.LOW)
     GPIO.output(MOTORR_P, GPIO.LOW)
 
+# go backwards with motors
+def go_fwd():
+    GPIO.output(MOTORL_N, GPIO.LOW)
+    GPIO.output(MOTORL_P, GPIO.HIGH)
+    GPIO.output(MOTORR_N, GPIO.LOW)
+    GPIO.output(MOTORR_P, GPIO.HIGH)
 
-init_gpio()
-go_fwd()
-time.sleep(10)
-stop_m()
-GPIO.cleanup()
+# move left
+def go_lft():
+    GPIO.output(MOTORL_N, GPIO.HIGH)
+    GPIO.output(MOTORL_P, GPIO.LOW)
+    GPIO.output(MOTORR_N, GPIO.LOW)
+    GPIO.output(MOTORR_P, GPIO.HIGH)
+
+# move right
+def go_rgt():
+    GPIO.output(MOTORL_N, GPIO.LOW)
+    GPIO.output(MOTORL_P, GPIO.HIGH)
+    GPIO.output(MOTORR_N, GPIO.HIGH)
+    GPIO.output(MOTORR_P, GPIO.LOW)
+
+# function that sets the direction of the motors
+def set_dir(dir):
+    if dir == "KEY_UP" or dir == "w":
+        go_fwd()
+    elif dir == " " or dir == "b":
+        stop_m()
+    elif dir == "KEY_LEFT" or dir == "a":
+        go_lft()
+    elif dir == "KEY_RIGHT" or dir == "d":
+        go_rgt()
+    elif dir == "KEY_DOWN" or dir == "s":
+        go_bwd()
+    else:
+        stop_m()
+
+
+def main():
+    init_gpio()
+    with Input(keynames='curses') as input_generator:
+        for e in input_generator:
+            set_dir(e)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        sys.exit()
