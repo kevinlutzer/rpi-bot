af30407ec2fc686c8f9219ab7cb1bfe17ffb711e
Kevin Lutzer <kml183@mail.usask.com>
2017-01-30 23:55:10 -0600

    changed ssh program so that it uses the serial port to communicate with the micro

--- ssh_control_motor/ssh_control_motor.py
+++ ssh_control_motor/ssh_control_motor.py
@@ -1,80 +1,36 @@
-# Author: Kevin Lutzer
-# Date: Dec 29 2016
-# Description: Script to control the rpi motors over ssh
-
-#imports
-import RPi.GPIO as GPIO
+import serial
 import time
 import sys
 from curtsies import Input
 
-#gpio pin connections to the h-bridge
-MOTORL_N = 5
-MOTORL_P = 6
-MOTORR_P = 13
-MOTORR_N = 19
-
-# function to init the gpio states as well as the BCM python module
-def init_gpio():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(MOTORL_N, GPIO.OUT)
-    GPIO.setup(MOTORL_P, GPIO.OUT)
-    GPIO.setup(MOTORR_N, GPIO.OUT)
-    GPIO.setup(MOTORR_P, GPIO.OUT)
-
-# function to make the robot go forward
-def go_bwd():
-    GPIO.output(MOTORL_N, GPIO.HIGH)
-    GPIO.output(MOTORL_P, GPIO.LOW)
-    GPIO.output(MOTORR_N, GPIO.HIGH)
-    GPIO.output(MOTORR_P, GPIO.LOW)
-
-# stop all motors from moving
-def stop_m():
-    GPIO.output(MOTORL_N, GPIO.LOW)
-    GPIO.output(MOTORL_P, GPIO.LOW)
-    GPIO.output(MOTORR_N, GPIO.LOW)
-    GPIO.output(MOTORR_P, GPIO.LOW)
-
-# go backwards with motors
-def go_fwd():
-    GPIO.output(MOTORL_N, GPIO.LOW)
-    GPIO.output(MOTORL_P, GPIO.HIGH)
-    GPIO.output(MOTORR_N, GPIO.LOW)
-    GPIO.output(MOTORR_P, GPIO.HIGH)
-
-# move left
-def go_lft():
-    GPIO.output(MOTORL_N, GPIO.HIGH)
-    GPIO.output(MOTORL_P, GPIO.LOW)
-    GPIO.output(MOTORR_N, GPIO.LOW)
-    GPIO.output(MOTORR_P, GPIO.HIGH)
-
-# move right
-def go_rgt():
-    GPIO.output(MOTORL_N, GPIO.LOW)
-    GPIO.output(MOTORL_P, GPIO.HIGH)
-    GPIO.output(MOTORR_N, GPIO.HIGH)
-    GPIO.output(MOTORR_P, GPIO.LOW)
+# set up serial object
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+time.sleep(2)
+if ser.isOpen():
+    print "Port is open and connection is made.\n"
 
 # function that sets the direction of the motors
 def set_dir(dir):
     if dir == "KEY_UP" or dir == "w":
-        go_fwd()
+        ser.write('o')
+        print 'fwd\n'
     elif dir == " " or dir == "b":
-        stop_m()
+        ser.write('_')
+        print 'stp\n'
     elif dir == "KEY_LEFT" or dir == "a":
-        go_lft()
+        ser.write('?')
+        print 'lft\n'
     elif dir == "KEY_RIGHT" or dir == "d":
-        go_rgt()
+        ser.write('O')
+        print 'rgt\n'
     elif dir == "KEY_DOWN" or dir == "s":
-        go_bwd()
+        ser.write('/')
+        print 'bwd\n'
     else:
-        stop_m()
+        ser.write('_')
 
-
+# main
 def main():
-    init_gpio()
     with Input(keynames='curses') as input_generator:
         for e in input_generator:
             set_dir(e)
@@ -83,5 +39,4 @@ if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        GPIO.cleanup()
         sys.exit()
